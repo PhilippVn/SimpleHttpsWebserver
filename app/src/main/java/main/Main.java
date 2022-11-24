e622bcfafc1ecf3153fefaa440a5f710d1eac881
Philipp von Neffe <philipp.von-neffe@tum.de>
2022-11-24 23:16:53 +0100

    updated gradle and project structure

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -1,2 +1,13 @@
-package Main;public class Main {
+package main;
+
+/**
+ * Main Class: Starts HTTPS Server
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Class c = Main.class;
+        String className = c.getName();
+        System.out.println("The fully-qualified name of the class is: " + className);
+    }
 }
